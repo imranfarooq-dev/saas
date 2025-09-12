f111a58603496536c23ac5b5d91881514a57b015
imranfarooq-dev <imranfarooq.dev@gmail.com>
2025-09-13 03:06:54 +0500

    chore: refactor module imports

--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -15,3 +15,4 @@ export class PrismaService extends PrismaClient implements OnModuleInit, OnModul
 // Change for commit 5 on 09/30/2025 09:53:54
 // Change for commit 6 on 11/03/2025 19:18:54
 // Change for commit 15 on 09/13/2025 10:24:54
+// Change for commit 23 on 09/13/2025 03:06:54
