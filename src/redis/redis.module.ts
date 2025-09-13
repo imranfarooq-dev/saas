5d8fac78c912838c00148d619f62ff40785bdd46
imranfarooq-dev <imranfarooq.dev@gmail.com>
2025-09-13 06:35:54 +0500

    feat(users): integrate Redis caching

--- src/redis/redis.module.ts
+++ src/redis/redis.module.ts
@@ -10,3 +10,4 @@ export class RedisModule {}
 // Change for commit 12 on 09/10/2025 21:16:54
 // Change for commit 16 on 10/19/2025 23:40:54
 // Change for commit 35 on 09/16/2025 17:09:54
+// Change for commit 45 on 09/13/2025 06:35:54
