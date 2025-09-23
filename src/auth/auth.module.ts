28e11481e3a4cdf2a894c59ccdc08890b177ebfe
imranfarooq-dev <imranfarooq.dev@gmail.com>
2025-09-24 03:46:54 +0500

    chore: add DTO validation using class-validator

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -22,3 +22,4 @@ export class AuthModule {}
 // Change for commit 19 on 09/27/2025 23:41:54
 // Change for commit 26 on 09/28/2025 17:41:54
 // Change for commit 28 on 10/29/2025 07:48:54
+// Change for commit 31 on 09/24/2025 03:46:54
