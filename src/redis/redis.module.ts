89de8d8eb6ac18fa8499403b83f8a170bade7ae9
imranfarooq-dev <imranfarooq.dev@gmail.com>
2025-09-29 04:11:15 +0500

    fix(auth): correct password hashing bug

--- src/redis/redis.module.ts
+++ src/redis/redis.module.ts
@@ -12,3 +12,4 @@ export class RedisModule {}
 // Change for commit 35 on 09/16/2025 17:09:54
 // Change for commit 45 on 09/13/2025 06:35:54
 // Change for commit 46 on 11/03/2025 03:50:54
+// Commit 9 - if (!user) throw new Error('Not found');
