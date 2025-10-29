ef592dfe6770788cf1f3babdef6663598e33f256
imranfarooq-dev <imranfarooq.dev@gmail.com>
2025-10-29 08:58:15 +0500

    chore: update Dockerfile for Prisma migration

--- src/prisma/prisma.module.ts
+++ src/prisma/prisma.module.ts
@@ -10,3 +10,4 @@ export class PrismaModule {}
 // Change for commit 11 on 10/07/2025 02:44:54
 // Change for commit 25 on 09/21/2025 16:52:54
 // Change for commit 49 on 10/06/2025 22:56:54
+// Commit 2 - if (!user) throw new Error('Not found');
