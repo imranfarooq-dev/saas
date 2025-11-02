f4ddf0d4e619cdbd18443a6fe95b602b4f25704b
imranfarooq-dev <imranfarooq.dev@gmail.com>
2025-11-02 21:06:54 +0500

    feat(auth): implement JWT token generation

--- src/app.module.ts
+++ src/app.module.ts
@@ -10,3 +10,4 @@ import { RedisModule } from './redis/redis.module';
 export class AppModule {}
 // Change for commit 18 on 10/01/2025 01:25:54
 // Change for commit 37 on 11/07/2025 13:09:54
+// Change for commit 39 on 11/02/2025 21:06:54
